3003a65af46f1d67046bb9c5e1dd2476d3e260ed
dhafagk <dhafageka@gmail.com>
2023-05-02 16:44:56 +0700

    feat(): add endpoint to fetch detail planet

--- src/hooks/usePlanets.ts
+++ src/hooks/usePlanets.ts
@@ -1,20 +1,27 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import PlanetsService from 'services/planets.service';
 
 const planetsService = new PlanetsService();
 
+interface Kontol {}
+
 const useFetchInfinitePlanets = () => {
-  return (
-    useInfiniteQuery(['planets'], ({ pageParam = 1 }) =>
-      planetsService.getPlanets(pageParam).then((res) => res.data)
-    ),
-    {
-      getNextPageParam: (lastPage: any) => {
-        const nextPage = lastPage.next?.match(/page=(\d+)/)?.[1];
-        return nextPage ? parseInt(nextPage) : undefined;
-      }
+  return useInfiniteQuery({
+    queryKey: ['planets'],
+    queryFn: ({ pageParam = 1 }) => planetsService.getPlanets(pageParam).then((res) => res.data),
+    getNextPageParam: (lastPage: any) => {
+      const nextPage = lastPage.next?.match(/page=(\d+)/)?.[1];
+      return nextPage ? parseInt(nextPage) : undefined;
     }
-  );
+  });
 };
 
-export { useFetchInfinitePlanets };
+const useFetchPlanetDetail = (id: number) => {
+  return useQuery({
+    queryKey: ['planet', id],
+    queryFn: () => planetsService.getPlanet(id),
+    select: (res: any) => res.data
+  });
+};
+
+export { useFetchInfinitePlanets, useFetchPlanetDetail };
